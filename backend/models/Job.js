e6e1c7ba051e70cf7472e951bc03fbefb27983af
Tushar Kumar <184378329+tushar73-jet@users.noreply.github.com>
2026-01-19 12:30:24 +0530

    update job.js

--- backend/models/Job.js
+++ backend/models/Job.js
@@ -1,11 +1,17 @@
 import mongoose from "mongoose"
 
 const jobSchema = new mongoose.Schema({
+    name:{
   type: String,
+  required:true},
+
   status: {
     type: String,
+    enum: ["pending", "completed", "failed"],
     default: "pending",
   },
-});
+},
+{ timestamps: true }
+);
 
 export default mongoose.model("Job", jobSchema)
