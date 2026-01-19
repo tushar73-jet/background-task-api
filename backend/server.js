50a4649c3e7440aa3a1f16867679420838f794dc
Tushar <tushar@Tushar-ka-MacBook-Pro.local>
2026-01-19 12:08:27 +0530

    connect to  mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Server is running");
