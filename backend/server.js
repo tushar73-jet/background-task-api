02724525d01b60f1aad5945efd9b0f8e2b2e7852
Tushar Kumar <184378329+tushar73-jet@users.noreply.github.com>
2026-02-12 09:21:33 +0530

    improvement

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db.js";
 import jobRoutes from "./routes/routes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -27,6 +28,8 @@ app.use("/api/jobs", jobRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
